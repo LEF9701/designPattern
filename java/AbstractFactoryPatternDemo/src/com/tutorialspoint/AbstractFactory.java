3665f889bbf96697663cee159f3f7760120c0131
Lawrence/Laurie <law41592@gmail.com>
2026-03-30 08:05:48 +0800

    Delete java/AbstractFactoryPatternDemo directory

--- java/AbstractFactoryPatternDemo/src/com/tutorialspoint/AbstractFactory.java
+++
@@ -1,5 +0,0 @@
-package com.tutorialspoint;
-
-public abstract class AbstractFactory {
-	abstract Shape getShape(String shapeType);
-}
--- java/AbstractFactoryPatternDemo/src/com/tutorialspoint/Shape.java
+++
@@ -1,5 +0,0 @@
-package com.tutorialspoint;
-
-public interface Shape {
-	void draw();
-}
